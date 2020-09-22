a7b91a4ca95f072f3743ad56ee49839940875f41
solodezaldivar <ramon.solodezaldivar@uzh.ch>
2020-09-22 15:13:15 +0200

    :tada:

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import styles from './App.module.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {Cards, Chart, CountryPicker} from './components';
+class App extends Component {
+  render(){
+    return (
+      <div className={styles.container}>
+        <Cards/>
+        <CountryPicker/>
+        <Chart/>
+      </div>
+    )
+  }
 }
+
 
 export default App;
